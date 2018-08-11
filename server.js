dba6f24a90b24032b45311e104721fa06a8c698e
Brauner Fernandes <braunerhot@hotmail.com>
2018-08-11 02:14:01 -0300

    Servidores Nodejs

---
+++ server.js
@@ -0,0 +1,17 @@
+
+const http = require('http')
+const port = 9595
+const port2 = 5050
+const ip = 'localhost'
+
+const server = http.createServer((req, res) => {
+  console.log('Recebendo uma request!')
+  console.log('URL', req.url)
+  res.end('<h1> Aqui fica o que vamos enviar para o navegador como resposta! </h1>')
+})
+
+server.listen(port, ip, () => {
+  console.log(`Servidor rodando em http://${ip}:${port}`)
+  console.log('Para derrubar o servidor: ctrl + c');
+})
+
